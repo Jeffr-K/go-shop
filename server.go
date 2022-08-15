708215ccf3e0a602d961f3564e4a60569bbbc707
smilejakdu <ash982416@gmail.com>
2022-08-15 14:57:48 +0900

    docker file and docker compose setting

--- server.go
+++ server.go
@@ -1,26 +1,40 @@
 package go_shop
 
 import (
+	"errors"
 	"fmt"
 	"github.com/labstack/echo/v4"
 	user "github.com/lambsroad/go-shop/users"
 	"log"
+	"net/http"
 )
 
-const _defaultPort = 8080
+const _defaultPort = 4000
 
 type Server struct {
 	userController user.Controller
 }
 
 func NewDefaultServer() *Server {
+	data := map[string]user.User{}
+	userRepository := user.NewRepository(data)
+	userService := user.NewService(*userRepository)
+	userController := user.NewController(*userService)
 	return &Server{
-		userController: *user.NewController(*user.NewService(user.DefaultSecret)),
+		userController: *userController,
 	}
 }
 
 func (s *Server) Run() {
 	e := echo.New()
+	e.HTTPErrorHandler = func(err error, context echo.Context) {
+		if errors.Is(err, user.ErrInvalidPassword) {
+			context.JSON(http.StatusBadRequest, map[string]string{"message": "invalid password"})
+			return
+		}
+		e.DefaultHTTPErrorHandler(err, context)
+	}
+	s.Routes(e)
 	log.Fatal(e.Start(fmt.Sprintf(":%d", _defaultPort)))
 }
 
