7999ccbd67e8aa304358373b7cd4eebe9cc8c0ef
smilejakdu <ash982416@gmail.com>
2022-08-16 20:32:54 +0900

    board domain

--- boards/dto/BoardRequest.go
+++ boards/dto/BoardRequest.go
@@ -1,1 +1,12 @@
 package dto
+
+type CreateBoardRequest struct {
+	Title   string `json:"title" example:"board title"`
+	Content string `json:"content"  example:"board content"`
+}
+
+type UpdateBoardRequest struct {
+	BoardId int    `json:"board_id" example:"board_id"`
+	Title   string `json:"update board title"`
+	Content string `json:"update board content"`
+}
